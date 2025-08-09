8184d4f3581eb068fb094f1f0be2b2da2498eea8
chrisburley01 <chrisburley01@gmail.com>
2025-08-09 18:14:20 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,6 @@
-const CACHE = 'omnia-cache-v1';
+const CACHE = "omnia-cache-v3";
+const BASE = "/Omnia-live/";
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-  // icons and any other local files can be added here, e.g. './icons/icon-192.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  const req = e.request;
-  // Try cache first, then network
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).catch(() => caches.match('./')))
-  );
-});
+  BASE,
+  BASE + "index.html",
+  BASE + "manifest
